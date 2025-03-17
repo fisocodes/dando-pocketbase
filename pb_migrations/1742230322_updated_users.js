3b147e3deebd17a9dcd518e924e2d8c0e4383ca2
Oscar Figueroa <fisocodes@gmail.com>
2025-03-17 09:55:42 -0700

    feat: defined user table with minimal fields

---
+++ pb_migrations/1742230322_updated_users.js
@@ -0,0 +1,74 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "avatarURL": "",
+        "name": ""
+      }
+    }
+  }, collection)
+
+  // remove field
+  collection.fields.removeById("text1579384326")
+
+  // remove field
+  collection.fields.removeById("file376926767")
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "avatarURL": "avatar",
+        "name": "name"
+      }
+    }
+  }, collection)
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1579384326",
+    "max": 255,
+    "min": 0,
+    "name": "name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "file376926767",
+    "maxSelect": 1,
+    "maxSize": 0,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "avatar",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": null,
+    "type": "file"
+  }))
+
+  return app.save(collection)
+})
